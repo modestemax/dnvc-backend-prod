6da968972b0924bbe91245c4c5acb9213bd596f0
ngDarkTracer <62553802+ngDarkTracer@users.noreply.github.com>
2021-09-23 16:56:47 +0200

    Add files via upload

---
+++ dashboard-dashboard-module.js
@@ -0,0 +1,97 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["dashboard-dashboard-module"],{
+
+/***/ "Oy4E":
+/*!*******************************************************!*\
+  !*** ./src/app/dashboard/dashboard-routing.module.ts ***!
+  \*******************************************************/
+/*! exports provided: DashboardRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DashboardRoutingModule", function() { return DashboardRoutingModule; });
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _dashboard_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./dashboard.component */ "QX6l");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+const routes = [{ path: '', component: _dashboard_component__WEBPACK_IMPORTED_MODULE_1__["DashboardComponent"] }];
+class DashboardRoutingModule {
+}
+DashboardRoutingModule.ɵfac = function DashboardRoutingModule_Factory(t) { return new (t || DashboardRoutingModule)(); };
+DashboardRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineNgModule"]({ type: DashboardRoutingModule });
+DashboardRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineInjector"]({ imports: [[_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"].forChild(routes)], _angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵsetNgModuleScope"](DashboardRoutingModule, { imports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]], exports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] }); })();
+
+
+/***/ }),
+
+/***/ "QX6l":
+/*!**************************************************!*\
+  !*** ./src/app/dashboard/dashboard.component.ts ***!
+  \**************************************************/
+/*! exports provided: DashboardComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DashboardComponent", function() { return DashboardComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var primeng_button__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! primeng/button */ "jIHw");
+/* harmony import */ var primeng_ripple__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! primeng/ripple */ "Q4Mo");
+
+
+
+class DashboardComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+}
+DashboardComponent.ɵfac = function DashboardComponent_Factory(t) { return new (t || DashboardComponent)(); };
+DashboardComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: DashboardComponent, selectors: [["app-dashboard"]], decls: 1, vars: 0, consts: [["pButton", "", "pRipple", "", "type", "button", "label", "Danger", 1, "p-button-raised", "p-button-danger"]], template: function DashboardComponent_Template(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](0, "button", 0);
+    } }, directives: [primeng_button__WEBPACK_IMPORTED_MODULE_1__["ButtonDirective"], primeng_ripple__WEBPACK_IMPORTED_MODULE_2__["Ripple"]], styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJkYXNoYm9hcmQuY29tcG9uZW50LnNjc3MifQ== */"] });
+
+
+/***/ }),
+
+/***/ "TDBs":
+/*!***********************************************!*\
+  !*** ./src/app/dashboard/dashboard.module.ts ***!
+  \***********************************************/
+/*! exports provided: DashboardModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DashboardModule", function() { return DashboardModule; });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _dashboard_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./dashboard-routing.module */ "Oy4E");
+/* harmony import */ var _dashboard_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./dashboard.component */ "QX6l");
+/* harmony import */ var _primeModule_primeng_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../primeModule/primeng.module */ "Gh5l");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+
+class DashboardModule {
+}
+DashboardModule.ɵfac = function DashboardModule_Factory(t) { return new (t || DashboardModule)(); };
+DashboardModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineNgModule"]({ type: DashboardModule });
+DashboardModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineInjector"]({ imports: [[
+            _angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+            _dashboard_routing_module__WEBPACK_IMPORTED_MODULE_1__["DashboardRoutingModule"],
+            _primeModule_primeng_module__WEBPACK_IMPORTED_MODULE_3__["PrimengModule"]
+        ]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵsetNgModuleScope"](DashboardModule, { declarations: [_dashboard_component__WEBPACK_IMPORTED_MODULE_2__["DashboardComponent"]], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+        _dashboard_routing_module__WEBPACK_IMPORTED_MODULE_1__["DashboardRoutingModule"],
+        _primeModule_primeng_module__WEBPACK_IMPORTED_MODULE_3__["PrimengModule"]] }); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=dashboard-dashboard-module.js.map
---
+++ mail-templates-mail-templates-module.js
@@ -0,0 +1,91 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["mail-templates-mail-templates-module"],{
+
+/***/ "ELPz":
+/*!************************************************************!*\
+  !*** ./src/app/mail-templates/mail-templates.component.ts ***!
+  \************************************************************/
+/*! exports provided: MailTemplatesComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MailTemplatesComponent", function() { return MailTemplatesComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+class MailTemplatesComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+}
+MailTemplatesComponent.ɵfac = function MailTemplatesComponent_Factory(t) { return new (t || MailTemplatesComponent)(); };
+MailTemplatesComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: MailTemplatesComponent, selectors: [["app-mail-templates"]], decls: 2, vars: 0, template: function MailTemplatesComponent_Template(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "mail-templates works!");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    } }, styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWlsLXRlbXBsYXRlcy5jb21wb25lbnQuc2NzcyJ9 */"] });
+
+
+/***/ }),
+
+/***/ "eAK1":
+/*!*********************************************************!*\
+  !*** ./src/app/mail-templates/mail-templates.module.ts ***!
+  \*********************************************************/
+/*! exports provided: MailTemplatesModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MailTemplatesModule", function() { return MailTemplatesModule; });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _mail_templates_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./mail-templates-routing.module */ "iqQ3");
+/* harmony import */ var _mail_templates_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./mail-templates.component */ "ELPz");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+class MailTemplatesModule {
+}
+MailTemplatesModule.ɵfac = function MailTemplatesModule_Factory(t) { return new (t || MailTemplatesModule)(); };
+MailTemplatesModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineNgModule"]({ type: MailTemplatesModule });
+MailTemplatesModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineInjector"]({ imports: [[
+            _angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+            _mail_templates_routing_module__WEBPACK_IMPORTED_MODULE_1__["MailTemplatesRoutingModule"]
+        ]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵsetNgModuleScope"](MailTemplatesModule, { declarations: [_mail_templates_component__WEBPACK_IMPORTED_MODULE_2__["MailTemplatesComponent"]], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+        _mail_templates_routing_module__WEBPACK_IMPORTED_MODULE_1__["MailTemplatesRoutingModule"]] }); })();
+
+
+/***/ }),
+
+/***/ "iqQ3":
+/*!*****************************************************************!*\
+  !*** ./src/app/mail-templates/mail-templates-routing.module.ts ***!
+  \*****************************************************************/
+/*! exports provided: MailTemplatesRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MailTemplatesRoutingModule", function() { return MailTemplatesRoutingModule; });
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _mail_templates_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./mail-templates.component */ "ELPz");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+const routes = [{ path: '', component: _mail_templates_component__WEBPACK_IMPORTED_MODULE_1__["MailTemplatesComponent"] }];
+class MailTemplatesRoutingModule {
+}
+MailTemplatesRoutingModule.ɵfac = function MailTemplatesRoutingModule_Factory(t) { return new (t || MailTemplatesRoutingModule)(); };
+MailTemplatesRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineNgModule"]({ type: MailTemplatesRoutingModule });
+MailTemplatesRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineInjector"]({ imports: [[_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"].forChild(routes)], _angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵsetNgModuleScope"](MailTemplatesRoutingModule, { imports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]], exports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] }); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=mail-templates-mail-templates-module.js.map
